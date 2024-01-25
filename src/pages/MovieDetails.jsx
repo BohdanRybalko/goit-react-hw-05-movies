1c35fc07377f92dbefb3734fdde48e5710e4fbc4
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2024-01-25 10:26:55 +0200

    fix poster

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { Link, Outlet, useParams } from 'react-router-dom';
 import { getMovieDetails } from '../services/api';
 
+const defaultImg = 'https://ireland.apollo.olxcdn.com/v1/files/0iq0gb9ppip8-UA/image;s=1000x700';
+
 const MovieDetails = () => {
   const { movieId } = useParams();
   const [movieDetails, setMovieDetails] = useState({});
@@ -23,6 +25,15 @@ const MovieDetails = () => {
     <div>
       <h2>{movieDetails.title}</h2>
       <p>{movieDetails.overview}</p>
+      {movieDetails.poster_path ? (
+        <img
+          src={`https://image.tmdb.org/t/p/w500/${movieDetails.poster_path}`}
+          width={250}
+          alt="Movie Poster"
+        />
+      ) : (
+        <img src={defaultImg} width={250} alt="Poster" />
+      )}
 
       <Link to="cast">Cast</Link>
       <Link to="reviews">Reviews</Link>
