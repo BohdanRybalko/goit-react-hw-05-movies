578d4ae3c58d135a38bd62a60aac2f0026e9e0fa
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2024-01-25 10:22:15 +0200

    fix actors

--- src/pages/Cast.jsx
+++ src/pages/Cast.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { getMovieCredits, getCastPhotos } from 'services/api';
+import { getMovieCredits, getCastPhotos } from '../services/api';
 import { useParams } from 'react-router-dom';
+
+const defaultImg = 'https://ireland.apollo.olxcdn.com/v1/files/0iq0gb9ppip8-UA/image;s=1000x700';
 
 const Cast = () => {
   const [cast, setCast] = useState([]);
@@ -27,11 +29,14 @@ const Cast = () => {
           <li key={actor.id}>
             <p>{actor.name}</p>
             <p>Character: {actor.character}</p>
-            {actor.id && (
+            {actor.profile_path ? (
               <img
-                src={`https://image.tmdb.org/t/p/w500${getCastPhotos(actor.id)}`}
-                alt={`Cast ${actor.name}`}
+                src={`https://image.tmdb.org/t/p/w500/${actor.profile_path}`}
+                width={250}
+                alt={`${actor.name} Photo`}
               />
+            ) : (
+              <img src={defaultImg} width={250} alt="Default Cast Member Photo" />
             )}
           </li>
         ))}
